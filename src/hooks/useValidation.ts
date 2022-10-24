09fd9c810e7e90e59e2dce59c4ecb80b8b3cbbe1
Mohamed-Sokar <109233779+Mohamed-Sokar@users.noreply.github.com>
2022-10-24 15:17:16 +0300

    validation password

---
+++ src/hooks/useValidation.ts
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react';
+
+const usePasswordValidation = (password: string) => {
+  const [atLeastEight, setAtLeastEight] = useState(false);
+  const [uppercase, setUppercase] = useState(false);
+  const [lowercase, setLowercase] = useState(false);
+  const [specialCharacter, setSpecialCharacter] = useState(false);
+
+  useEffect(() => {
+    /.{8,}/.test(password) ? setAtLeastEight(true) : setAtLeastEight(false);
+    /(?=.*?[A-Z])/.test(password) ? setUppercase(true) : setUppercase(false);
+    /(?=.*?[a-z])/.test(password) ? setLowercase(true) : setLowercase(false);
+    /(?=.*?[#?!@$%^&*-])/.test(password)
+      ? setSpecialCharacter(true)
+      : setSpecialCharacter(false);
+
+    return () => {
+      setAtLeastEight(false);
+      setUppercase(false);
+      setLowercase(false);
+      setSpecialCharacter(false);
+    };
+  }, [password]);
+
+  const output = [atLeastEight, uppercase, lowercase, specialCharacter];
+  const isValid = output.every(item => item === true);
+
+  return {atLeastEight, uppercase, lowercase, specialCharacter, isValid};
+};
+
+export default usePasswordValidation;
